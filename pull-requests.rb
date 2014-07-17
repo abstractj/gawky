d7cbacb36c2aa0f7ab6b60006b461b9c95d76bc4
Bruno Oliveira <bruno@abstractj.org>
2014-07-17 14:48:04 -0300

    Initial dirty version

---
+++ pull-requests.rb
@@ -0,0 +1,15 @@
+require 'github_api'
+require 'term/ansicolor'
+
+connection = Github.new oauth_token: 'c2a21e0dbea1d0f3f5d7a1821b63ed55f4d87f38'
+
+pulls = 0
+issues = connection.issues.list(:org => 'aerogear', :filter => 'all', :auto_pagination => true)
+issues.each do |issue|
+  date = DateTime.parse(issue.created_at)
+  puts "#{date.strftime('%b %e, %l:%M %p')} - \e[34m#{issue.user.login}\e[0m - \e[33m#{issue.title}\e[0m - \e[34m#{issue.html_url}\e[0m"
+  if issue["pull_request"]
+    pulls += 1
+  end
+end
+
