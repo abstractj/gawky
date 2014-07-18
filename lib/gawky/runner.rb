e61a855c4cd02aecf35aa08d531333be56fee719
Bruno Oliveira <bruno@abstractj.org>
2014-07-18 13:05:45 -0300

    Now really return all the pull requests opened

--- lib/gawky/runner.rb
+++ lib/gawky/runner.rb
@@ -6,23 +6,23 @@ module Gawky
     end
 
     def run
-      issues = Gawky.github.issues.list(:org => ARGV[0],
-                                        :filter => 'all',
-                                        :auto_pagination => true,
-                                        :sort => 'created')
-      issues.each do |issue|
-        if issue['pull_request']
-          date = DateTime.parse(issue.created_at)
-          if elapsed_time(date) > Gawky.max_elapsed_days
-            puts Status.new(date, issue).warning
-          else
-            puts Status.new(date, issue).green
+      repos = Gawky.github.repos.list org: ARGV[0], per_page: 100
+      repos.each do |repo|
+        if repo.open_issues_count > 0
+          pulls = Gawky.github.pull_requests.list user: ARGV[0], repo: repo.name
+          pulls.each do |pull|
+            date = DateTime.parse(pull.created_at)
+            if elapsed_time(date) > Gawky.max_elapsed_days
+              puts Status.new(date, pull).warning
+            else
+              puts Status.new(date, pull).green
+            end
           end
         end
       end
     end
 
-  private
+    private
 
     def elapsed_time(date)
       (@date_now - date).to_i
