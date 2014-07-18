c88adf3f53b773464a3dd65f8eb3ea6dc989796a
Bruno Oliveira <bruno@abstractj.org>
2014-07-18 00:52:23 -0300

    Extract runner code and make it a bit clean

--- lib/gawky.rb
+++ lib/gawky.rb
@@ -1,38 +1,19 @@
 require 'gawky/version'
+require 'gawky/runner'
+require 'gawky/status'
 require 'github_api'
-require 'status'
 require 'json'
-
 
 module Gawky
 
-  class Runner
+  @@configuration = JSON.load(IO.read(ENV['HOME'] + '/.gawky.json'))
 
-    def initialize
-      @date_now = Date.today
-      configuration = JSON.load(IO.read(ENV['HOME'] + '/.gawky.json'))
-      @github = Github.new oauth_token: configuration['token']
-      @acceptable = configuration['maxElapsedDays']
-    end
-
-
-    def run
-      issues = @github.issues.list(:org => ARGV[0], :filter => 'all', :auto_pagination => true, :sort => 'created')
-      issues.each do |issue|
-        if issue['pull_request']
-          date = DateTime.parse(issue.created_at)
-          if elapsed_time(date) > @acceptable
-            puts Status.new(date, issue).warning
-          else
-            puts Status.new(date, issue).green
-          end
-        end
-      end
-    end
-
-    private
-    def elapsed_time(date)
-      (@date_now - date).to_i
-    end
+  def self.github
+    Github.new oauth_token: @@configuration['token']
   end
+
+  def self.max_elapsed_days
+    @@configuration['maxElapsedDays']
+  end
+
 end
